590caf842ad09074e2de32041880b55d51cb32fd
Jay Francis <jay.francis@justdevelop.it>
2013-08-09 14:32:57 +0100

    Instantiate a WebVTT object, add captions and call render()

---
+++ src/WebVTT/Caption.php
@@ -0,0 +1,85 @@
+<?php
+namespace WebVTT;
+
+class Caption
+{
+  private $_startSecond;
+  private $_endSecond;
+  private $_text;
+
+  function __construct($startSecond, $endSecond, $text)
+  {
+    $this
+    ->setStartSecond($startSecond)
+    ->setEndSecond($endSecond)
+    ->setText($text);
+  }
+
+  public function render()
+  {
+    $startTime = gmdate("i:s.000", $this->getStartSecond());
+    $endTime = gmdate("i:s.000", $this->getEndSecond());
+
+    $caption = sprintf(
+      '%s --> %s%s%s%s',
+      $startTime,
+      $endTime,
+      PHP_EOL,
+      $this->getText(),
+      PHP_EOL
+    );
+
+    return $caption;
+  }
+
+  /**
+   * @param $startSecond
+   *
+   * @return Caption
+   */
+  public function setStartSecond($startSecond)
+  {
+    $this->_startSecond = $startSecond;
+
+    return $this;
+  }
+
+  public function getStartSecond()
+  {
+    return $this->_startSecond;
+  }
+
+  /**
+   * @param $endSecond
+   *
+   * @return Caption
+   */
+  public function setEndSecond($endSecond)
+  {
+    $this->_endSecond = $endSecond;
+
+    return $this;
+  }
+
+  public function getEndSecond()
+  {
+    return $this->_endSecond;
+  }
+
+  /**
+   * @param $text
+   *
+   * @return Caption
+   */
+  public function setText($text)
+  {
+    $this->_text = $text;
+
+    return $this;
+  }
+
+  public function getText()
+  {
+    return $this->_text;
+  }
+}
---
+++ src/WebVTT/WebVTT.php
@@ -0,0 +1,36 @@
+<?php
+namespace WebVTT;
+
+class WebVTT
+{
+  /** @var Caption[] */
+  private $_captions = array();
+
+  public function addCaption($startSecond, $ensSecond, $text)
+  {
+    $this->_captions[] = new Caption($startSecond, $ensSecond, $text);
+
+    return $this;
+  }
+
+  /**
+   * @return Caption[]
+   */
+  public function getCaptions()
+  {
+    return $this->_captions;
+  }
+
+  public function renderCaptions()
+  {
+    $captions = ['WEBVTT'];
+
+    foreach($this->getCaptions() as $caption)
+    {
+      $captions[] = $caption->render();
+    }
+
+    header("Content-Type:text/vtt;charset=utf-8");
+    echo implode(PHP_EOL, $captions);
+  }
+}
